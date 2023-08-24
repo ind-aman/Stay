4e21bf3fabf82dc57c720b8636ed00ea051366fc
Aman Kumar <jkuma917@jci.com>
2023-08-25 02:37:05 +0530

    trying to merge Branch

--- Helper/src/app/app.module.ts
+++ Helper/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DropdownComponent } from './dropdown/dropdown.component';
+import { DropdownComponent } from './Component/dropdown/dropdown.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
